4ffb65cc4be6c0d03cf051950b9e79cd8211cb78
Xavier Salazar <contact@xsalazar.com>
2025-08-12 10:35:11 -0700

    Bump `node` version

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "instance" {
   filename      = "${path.module}/dummy-lambda-package/lambda.zip" // Simple hello world application
   role          = aws_iam_role.instance.arn
   handler       = "index.handler"
-  runtime       = "nodejs20.x"
+  runtime       = "nodejs22.x"
   timeout       = 30   // seconds
   memory_size   = 1536 // MB
 
